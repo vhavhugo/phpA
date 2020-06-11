b806cf1ae79983701e707c012526185c5767bdce
vhavhugo <vhavhugo@hotmail.com>
2020-06-11 17:46:40 -0300

    POO - Herança

--- 3-projeto/src/Titular.php
+++ 3-projeto/src/Titular.php
@@ -1,9 +1,7 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private $cpf;
-    private $nome;
     private $endereco;
 
     public function __construct(string $cpf, string $nome, Endereco $endereco)
@@ -11,29 +9,12 @@ class Titular
         $this->cpf = $cpf;
         $this->validaNomeTitular($nome);
         $this->nome = $nome;
+        $this->endereco = $endereco;
     }
-
-    public function validaNomeTitular(string $nomeTitular)
-    {
-        if(strlen($nomeTitular) < 5){
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
-    }
-
-    public function recuperaCpf(): string
-    {
-        return $this->cpf;
-    }
-
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
-
+    
     public function recuperaEndereco(): Endereco
     {
         return $this->endereco;
     }
-   
+    
 }
