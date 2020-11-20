2e3049ede180cff050d4c99314d8726b95ff5b9a
vhavhugo <vhavhugo@hotmail.com>
2020-11-20 06:34:57 -0300

    finalizando o widget

--- wordpress/plugin-wp/wordpress/wp-content/plugins/al_patrocinadores_alura/includes/al_patrocinadores_palestras_widget.php
+++ wordpress/plugin-wp/wordpress/wp-content/plugins/al_patrocinadores_alura/includes/al_patrocinadores_palestras_widget.php
@@ -20,30 +20,51 @@ class PatrocinadoresAlura extends WP_Widget{
         ?>
         <p>
             <input type="checkbox" id="<?= $this->get_field_id('caelum') ?>"
-            name="<?= $this->get_field_id('caelum') ?>" 
+            name="<?= $this->get_field_name('caelum') ?>" 
             value="1">
             <label for="<?= $this->get_field_id("caelum")?>">Caelun</label>
         </p>
         <p>
             <input type="checkbox" id="<?= $this->get_field_id('casa_do_codigo') ?>"
-            name="<?= $this->get_field_id('casa_do_codigo') ?>" 
+            name="<?= $this->get_field_name('casa_do_codigo') ?>" 
             value="1">
             <label for="<?= $this->get_field_id("casa_do_codigo")?>">Casa do Código</label>
         </p>
         <p>
             <input type="checkbox" id="<?= $this->get_field_id('hipters') ?>"
-            name="<?= $this->get_field_id('hipters') ?>" 
+            name="<?= $this->get_field_name('hipters') ?>" 
             value="1">
             <label for="<?= $this->get_field_id("hipters")?>">Hipters</label>
         </p>
         <?php
     }
 
-    public function update($new_instance, $old_stance){
+    public function update($new_instance, $old_stance)
+    {
         $instance = array();
         $instance['caelum'] = !empty($new_instance['caelum']) ? strip_tags($new_instance['caelum']) : '';
         $instance['casa_do_codigo'] = !empty($new_instance['casa_do_codigo']) ? strip_tags($new_instance['casa_do_codigo']) : '';
         $instance['hipters'] = !empty($new_instance['hipters']) ? strip_tags($new_instance['hipters']) : '';
         return $instance;
+    }
+
+    public function widget($args, $instance)
+    {
+        ?>
+            <section class="patrocinadores-principais">
+                <h3 class="titulo-patrocinadores">Patrocinadores</h3>
+                    <ul class="lista-patrocinadores">
+                        <?php if(!empty($instance['caelum'])): ?>
+                            <li><img src="<?= plugin_dir_url(__FILE__) . '../imagens/caelum.png' ?>"/></li>
+                        <?php endif; ?>
+                        <?php if(!empty($instance['casa_do_codigo'])): ?>
+                            <li><img src="<?= plugin_dir_url(__FILE__) . '../imagens/casa-do-codigo.png' ?>"/></li>
+                        <?php endif; ?>
+                        <?php if(!empty($instance['hipters'])): ?>
+                            <li><img src="<?= plugin_dir_url(__FILE__) . '../imagens/hipters.png' ?>"/></li>
+                        <?php endif; ?>
+                    </ul> 
+            </section>
+        <?php
     }
 }
