5f39d86a50739a38a1d7fde207347a3193fc1fbe
vhavhugo <vhavhugo@hotmail.com>
2020-12-27 18:19:21 -0300

    woocommerce

---
+++ wordpress/plugin-wp/wordpress/wp-content/plugins/al_local_dia_palestra/al_local_dia_palestra.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package Cadastrar local e data da palestra
+ */
+/*
+Plugin Name: Cadastrar local e data da palestra
+Description: Plugin para cadastrar local e data da próxima palestra realizada pela Alura.
+Author: Hugo do Valle
+Version: 1.0.0
+*/
+
+ if(!defined('ABSPATH')){
+    die;
+ }
