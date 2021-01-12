1abab4aad498bfb04881f3c84b8c09c9f6034f1f
vhavhugo <vhavhugo@hotmail.com>
2021-01-12 14:57:37 -0300

    Construindo um tema wordpress2

---
+++ wordpress-tema/wp-content/themes/alura_intercambios/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title><?php bloginfo('name') ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        <?php wp_head(); ?>
+    </head>
+    <body>
+    
+    </body>
+</html>
---
+++ wordpress-tema/wp-content/themes/alura_intercambios/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'header.php';
+?>
+<h1>Olá mundo</h1>
+<?php
+require_once 'footer.php';
+?>
