1f4191457b4863d54d71b29a28545a61ee9db956
vhavhugo <vhavhugo@hotmail.com>
2021-01-12 16:23:06 -0300

    css do site

--- wordpress-tema/wp-content/themes/alura_intercambios/header.php
+++ wordpress-tema/wp-content/themes/alura_intercambios/header.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-    <head>
-        <title><?php bloginfo('name') ?></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-        <?php wp_head(); ?>
-    </head>
-    <body>
-    <?php 
-    the_custom_logo();
-    wp_nav_menu(array(
-        'menu' => 'menu-navegacao'
-    )) 
-    
-    ?> 
+
+<head>
+    <title><?php bloginfo('name') ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+    <?php wp_head(); ?>
+    <link href="<?= get_template_directory_uri() . '/css/normalize.css' ?> " rel="stylesheet">
+    <link href="<?= get_template_directory_uri() . '/css/bootstrap.css' ?> " rel="stylesheet">
+    <link href="<?= get_template_directory_uri() . '/css/header.css' ?> " rel="stylesheet">
+</head>
+
+<body <?php body_class(); ?>>
+    <header class="site-header">
+        <div class="container-alura">
+            <?php
+            the_custom_logo();
+            ?>
+        </div>
+        <nav>
+            <?php
+            wp_nav_menu(array(
+                'menu'    => 'menu-navegacao',
+                'menu_id' => 'menu-principal'
+            ))
+
+            ?>
+        </nav>
+    </header>
