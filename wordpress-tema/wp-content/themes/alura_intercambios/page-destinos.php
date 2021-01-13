4473fd1aa87997ed854d55da3166c279680b298f
vhavhugo <vhavhugo@hotmail.com>
2021-01-12 22:53:32 -0300

    pagina destinos

--- wordpress-tema/wp-content/themes/alura_intercambios/page-destinos.php
+++ wordpress-tema/wp-content/themes/alura_intercambios/page-destinos.php
@@ -1,1 +1,21 @@
-<h2>Esse é o arquivo da pagina destino</h2>
+<?php
+$estiloPagina = 'destinos.css';
+require_once 'header.php';
+
+$args = array('post_type' => 'destinos');
+$query = new WP_Query($args);
+
+if ($query->have_posts()) :
+    echo '<main class="page-destinos">';
+    echo '<ul class="lista-destinos container-alura">';
+    while ($query->have_posts()) : $query->the_post();
+    echo '<li class="col-md-3 destinos">';    
+    the_post_thumbnail();
+        the_title('<p class="titulo-destino">','</p>');
+        the_content();
+        echo '</li>';
+    endwhile;
+    echo '</ul>';
+    echo '</main>';
+endif;
+require_once 'footer.php';
