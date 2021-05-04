3807da3da18050c4fc4534588f8f9f6370d67aa6
vhavhugo <vhavhugo@hotmail.com>
2021-05-04 18:40:34 -0300

    erros e logs

---
+++ exceptions/php-exceptions-projeto-inicial/erros.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(E_All);
+
+init_set('display_errors', 1); // liga e desliga - mostrar erros
+init_set('log_errors', 1); // Ativar log dos erros = 1
+init_set('error_log', '/var/log/minha-apicacao'); // O caminho dos logs
+
+
+echo $variavel;
+echo $array[12];
+
+echo CONSTANTE;
