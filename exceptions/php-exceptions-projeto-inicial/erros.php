a99c30a8eca1e44a9a5da4b240f1f5a2c5885463
vhavhugo <vhavhugo@hotmail.com>
2021-05-04 19:11:10 -0300

    Tratamento de erros

--- exceptions/php-exceptions-projeto-inicial/erros.php
+++ exceptions/php-exceptions-projeto-inicial/erros.php
@@ -3,9 +3,19 @@
 error_reporting(E_All);
 
 init_set('display_errors', 1); // liga e desliga - mostrar erros
-init_set('log_errors', 1); // Ativar log dos erros = 1
-init_set('error_log', '/var/log/minha-apicacao'); // O caminho dos logs
+//init_set('log_errors', 1); // Ativar log dos erros = 1
+//init_set('error_log', '/var/log/minha-apicacao'); // O caminho dos logs
 
+set_error_handler(function($errno, $errstr, $errfile, $errline){
+    switch($errno){
+        case E_WARNING:
+            echo "Aviso: Isso é perigoso";
+            break;
+        case E_NOTICE:
+            echo "Melhor não fazer isso";
+            break;
+    }
+});
 
 echo $variavel;
 echo $array[12];
